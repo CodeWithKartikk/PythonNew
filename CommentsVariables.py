02ad2b2dc3aeff9b46b13a09a4ec99691717cd9f
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-28 02:56:37 +0530

    Python Comments and Variables

---
+++ CommentsVariables.py
@@ -0,0 +1,95 @@
+'''
+Author : Kartik Kathuria
+Topic : Learning Python
+Time : 12:23
+'''
+
+# Python is a high level programming language which is easy to understand.
+# Developer's First Choice
+# Developed byGuido Van Rossum in 1991
+
+# .py extension in python
+# abc.pdf   (for pdf we have .pdf extension)
+# avs.word  (for word we have .word extension)
+# abc.py    (.py is an extension for python file)
+
+# Comments
+# 1. Single Line comment
+
+print("Single line Comment") # Single Line comment 
+"""  2. Multi 
+     Line 
+     Comment """
+
+# Kartik Kathuria
+'''Starting Triple Quote Content Ending Triple quote'''
+
+"""
+Ajhvfjkl
+vkjvjkjvkf
+hjghjhvb
+nvfgvfh
+bhbfbhjdb
+"""
+
+''' Python Variables '''
+num1 = 6 #Declaring and Initialising num1
+num2 = 7 #Declaring and Initialising num2
+sum = num1 + num2
+print(sum)
+int myVariable = 6;
+
+x = 5 # x is of type integer
+y = "Avinash" # y is of type String
+c = 'a'
+z = 8.59 # z is of float type
+
+Casting : Casting is basically converting our datatype to any type whatever you want.
+
+p = str(3)
+q = int(5)
+floatVar = float(x)
+intVar = int(z)
+print(m) # ord() and chr() --> Functions to change into ascii and int
+print(intVar)
+variableName (CamelCasing Rule) (Type Convention)
+
+print(floatVar)
+
+# WAP to print the simple interest
+p, r, t = 5, 5, 5 # p is the principal amt in int dataType
+print("The simple interest is:" + (p*r*t)/100)
+
+myVar  = "John"
+my_Var = "John"
+MY_VAR = "John"
+
+# These variable names are against the rules of identifiers
+2myVar = "john"
+my-var = "john"
+my var = "john"
+
+'''Naming Conventions of variables, methods'''
+# myVariable       --> camelCase
+# MyVariable       --> PascalCase
+# my_variable_name --> snakecase
+
+''' Assign Multiple values in my variable in different lines '''
+p = 5
+q = 7
+r = 9
+print(p)
+print(q)
+print(r)
+
+''' Assign Multiple values in my variable in same line'''
+x, y, z = "kartik,", "Abhishek,", "Prerita" # assigning multiple
+print(x,y,z)
+
+''' list is used for storing multiple valuess''' 
+# unpack a list
+names = ["kartik", "Abhishek", "Prerita"]
+print(names)
+
+# my_float_variable
+# myFloatVariable
