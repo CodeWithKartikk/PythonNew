b657c6dee06df020736e9765bf6de0a60c802b00
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-08-07 18:57:17 +0530

    User Input initial commit

---
+++ UserInput.py
@@ -0,0 +1,32 @@
+''' How can i take input from user in integer '''
+
+# input() -> input function is basically used to take any imput from user
+
+# Firstly if i want to take input from the user so i have to specify which type of data i want to take as an input 
+
+'''Integer'''
+n1 = int(input("Enter your first number: "))
+n2 = int(input("Enter your second number: "))
+c = n1 + n2
+print(c)
+
+'''Float'''
+n1 = float(input("Enter your first number: "))
+n2 = float(input("Enter your second number: "))
+c = n1 + n2
+print(c)
+
+'''Complex'''
+n1 = complex(input("Enter your first number: "))
+n2 = complex(input("Enter your second number: "))
+c = n1 + n2
+print(c)
+
+'''Eval'''
+n1 = eval(input("Enter your first number: "))
+n2 = eval(input("Enter your second number: "))
+c = n1 + n2
+print(c)
+
+str = input("Enter your Name: ")
+print(str)
