f200dc318910a693983a10312e366df2493a08b6
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-09-05 16:27:55 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,97 @@
+''' 
+Author: Kartik Kathuria
+Class: Python Operators
+Time - 15:23
+'''
+
+# Operators are used to perform operations on variables and values
+
+# print(10+5)
+
+'''
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+5. Identity Operators
+6. Membership Operator
+7. Bitise Operator
+'''
+
+'''Python Arithmetic Operators'''
+# x = 5
+# y = 4
+
+# 1. Addition
+# print("Addition")
+# print(x+y)
+
+# 2. Subtraction
+# print("Subtraction")
+# print(x-y)
+
+# 3. Multiplication
+# print("Multiplication")
+# print(x*y)
+
+# 4. Division
+# print("Division")
+# print(x/y)
+
+# 5. Floor Division
+# print("Floor division")
+# print(x//y)
+
+# 6. Modulus
+# print("Modulus")
+# print(x%y)
+
+# 7. Exponentiation
+# print("Exponentiation")
+# print(x**y)
+
+'''Python Assignment Operators'''
+x = 5
+# print(x)
+
+# x += 3 
+# print(x)
+
+# x -= 3
+# print(x)
+
+# x *= 7
+# print(x) 
+
+# x /= 5
+# print(x)
+
+# x %= 4
+# print(x)
+
+# # x //= 3***
+# # print(x)
+
+# x **= 3
+# print(x)
+
+# x//=5
+# print(x)
+
+# x &= 3
+# print(x)
+
+# x |= 3 ***
+# print(x)
+
+# x ^= 3
+# print(x)
+
+# x >>= 3 ***
+# print(x)
+
+# x <<= 3 ***
+# print(x)
+
+'''Python Comparison Operators'''
+# ==, !=, >, <, >=, <=
